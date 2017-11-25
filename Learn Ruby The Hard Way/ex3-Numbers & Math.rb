a4c6835f9235f09721d1fdd457bbcb41b86c9103
Vinil Pillai <thezephyrus@users.noreply.github.com>
2017-11-25 15:31:37 +1100

    ex 3 math & numbers

---
+++ Learn Ruby The Hard Way/ex3-Numbers & Math.rb
@@ -0,0 +1,40 @@
+# Lets see what ruby has to offer in Math
+
+puts " Using Integers"
+puts "\nI will now count my chickens"
+puts "Hens #{25+30/6}"
+puts "Roosters #{100-25*3%4}"
+
+puts "\nNow i will count the eggs"
+puts 3+2+1-5+4%2-1/4+6
+
+puts "\nIs it true that 3+2<5-7?"
+puts 3+2<5-7
+
+puts "whats 3+2? #{3+2}"
+puts "Whats 5-7? #{5-7}"
+
+puts "Oh! that's why it is false."
+
+puts "\nHow about some more."
+
+puts "Is it greater? #{5>-2}"
+puts "Is it greater or equal? #{5>=2}"
+puts "Is it less or equal? #{5<=-2}"
+
+puts "\n\n Floating numbers"
+
+puts "\nI will now count my chickens"
+puts "Hens #{25.0+30/6}"
+puts "Roosters #{100.0-25*3%4}"
+
+puts "\nNow i will count the eggs"
+puts 3+2+1-5+4%2-1/4+6.0
+
+puts "\nIs it true that 3+2<5-7?"
+puts 3+2<5.0-7
+
+puts "whats 3+2? #{3.0+2}"
+puts "Whats 5-7? #{5-7.0}"
+
+puts "Oh! that's why it is false."
