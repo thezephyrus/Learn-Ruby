8f220fc3d3a131cf9f841907acdfeb6a765d5c94
Vinil Pillai <thezephyrus@users.noreply.github.com>
2017-11-25 15:11:53 +1100

    Starting off with Learn Ruby The Hard Way

---
+++ Learn Ruby The Hard Way/ex2-Comments & Pound characters.rb
@@ -0,0 +1,9 @@
+# A comment, this is you can read the program later.
+# Anything after # is ignored by Ruby
+
+puts "I could have codes like this." # and the comment after is ignored
+
+# You can also use # to comment a part of code
+#puts "This will not run."
+
+puts "But this will run" 
